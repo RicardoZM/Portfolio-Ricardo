a5c2323eb4fc9db45a6d58e38fe2604fdf2f7c6f
RicardoZM <ricardo_zorita@hotmail.com>
2023-08-12 19:03:44 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+function App() {
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>{" "}
+    </div>
+  );
+}
+
+export default App;
